826614f1ab95795121e1a7c8d5e5e7b4b99eecb3
DEEPAK GADWAL <deepak gadwal>
2023-02-25 17:59:31 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+let menu =document.querySelector('#menu-icon')
+let navmenu =document.querySelector('.navmenu')
+
+
+menu.onclick=()=>{
+    menu.classList.toggle('bx-x')
+    navmenu.classList.toggle('navmenu-open')
+}
